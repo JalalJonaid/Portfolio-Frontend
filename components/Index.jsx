fa8bad7f259caf2c62cb52e36fbb237363079d8c
Jalal Jonaid <jalaljonaid@jalals-air.myfiosgateway.com>
2023-12-03 10:44:25 -0500

    component fixes

---
+++ components/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import MatchupCard from './MatchupCard';
+const API = import.meta.env.VITE_BASE_URL;
+
+export default function Index() {
+  return (
+    <div>Index</div>
+  )
+}
---
+++ components/MatchupCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MatchupCard( {character, id }) {
+
+  return (
+    <div className="matchup-card">
+      
+    </div>
+  )
+}
